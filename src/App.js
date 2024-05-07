d49fef56b2bc118af66161f55451c0d047528009
louay <louay.badri@insat.ucar.tn>
2024-05-07 18:17:09 +0100

    map FOR BATCH IS READYYYYYYYYY

--- src/App.js
+++ src/App.js
@@ -2,34 +2,28 @@ import './App.css';
 import { Navbar, Nav } from 'react-bootstrap';
 import { useState } from 'react';
 import StreamChart from './components/chart_stream';
+import BatchChart from './components/chart_batch';
 import MapChart from './components/map';
 function App() {
-  const [batchOrSteam, setBatchOrSteam] = useState("Batch")
+  const [batchOrSteam, setBatchOrSteam] = useState("BatchChart")
 
   return (
     <div className="App">
       <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
 
       <div style={{ height: "50px", display: "flex", alignItems: "center", justifyItems: "center" }}>
-        <Nav className="mr-auto" >
-          <Nav.Link href="#home" onClick={
-            () => {
-              setBatchOrSteam("Batch")
-            }
-          } >Batch</Nav.Link>
-          <Nav.Link href="#link"
-            onClick={
-              () => {
-                setBatchOrSteam("Stream")
-              }
-            }
-          >Steam</Nav.Link>
+        <Nav className="mr-auto flex column align-items-center w-100" style={{ justifyContent: "space-evenly" }}>
+          <div onClick={() => { setBatchOrSteam("StreamChart") }} >StreamChart</div>
+          <div onClick={() => { setBatchOrSteam("StreamMap") }} >StreamMap</div>
+          <div onClick={() => { setBatchOrSteam("BatchChart") }} >BatchChart</div>
+          <div onClick={() => { setBatchOrSteam("BatchMap") }}>BatchMap</div>
         </Nav>
 
       </div>
-      {/* {batchOrSteam === "Stream" && <StreamChart />}
-      {batchOrSteam === "Batch" && <BatchChart />} */}
-      <MapChart />
+      {batchOrSteam === "StreamChart" && <StreamChart />}
+      {batchOrSteam === "StreamMap" && <StreamChart />}
+      {batchOrSteam === "BatchChart" && <BatchChart />}
+      {batchOrSteam === "BatchMap" && <MapChart />}
     </div>
   );
 }
--- src/components/map.jsx
+++ src/components/map.jsx
@@ -8,7 +8,7 @@ import {
     Sphere,
     Graticule,
 } from "react-simple-maps";
-
+import toast, { Toaster } from 'react-hot-toast';
 const geoUrl = "../../features.json";
 
 
@@ -17,7 +17,7 @@ const MapChart = () => {
     function getRedIntensityColor(value, min, max) {
         // Normalize the value to a number between 0 and 1
         var normalizedValue = 1 - (value - min) / (max - min);
-
+        //get the integer part of the normalized value
         // Convert the normalized value to a hexadecimal color code
         var color =
             "#FF" +
@@ -34,23 +34,32 @@ const MapChart = () => {
     const [maxValue, setMaxValue] = useState(0);
     const [minValue, setMinValue] = useState(0);
     const [chart, setChart] = useState(null);
-    // useEffect(() => {
-    //     async function fetchAllData() {
-    //         const response = await axios.get("http://localhost:3005/batch/all");
-    //         // console.log(response);
-    //         if (response.data.success) {
-    //             // console.log(response.data.value);
-    //             setAllData(response.data.value);
-    //         } else {
-    //             console.log('====================================');
-    //             console.log("Error");
-    //             console.log('====================================');
-    //         }
-    //     }
-    //     fetchAllData();
-    // }, []);
-
-    return (
+    useEffect(() => {
+        async function fetchAllData() {
+            const response = await axios.get("http://localhost:3005/batch/all");
+            // console.log(response);
+            if (response.data.success) {
+                console.log(response.data.value);
+                //get max value 
+                const max = Math.max(...response.data.value.map(item => item.average_temperature));
+                const min = Math.min(...response.data.value.map(item => item.average_temperature));
+                console.log(max, min);
+                setMaxValue(max);
+                setMinValue(min);
+                setAllData(response.data.value);
+            } else {
+                console.log('====================================');
+                console.log("Error");
+                console.log('====================================');
+            }
+        }
+        fetchAllData();
+    }, []);
+    const notify = () => toast('Here is your toast.', {
+        duration: 4000,
+        position: 'top-right',
+    });
+    return (<div><Toaster />
         <ComposableMap
             projectionConfig={{
                 rotate: [-10, 0, 0],
@@ -59,31 +68,65 @@ const MapChart = () => {
         >
             <Sphere stroke="#E4E5E6" strokeWidth={0.5} />
             <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
-            {data.length > 0 && (
-                <Geographies geography={geoUrl}>
-                    {
-                        ({ geographies }) => {
-                            console.log(geographies);
-                            return geographies.map((geo, index) => {
-                                // const value = allData[index];
-                                console.log(geo);
+            <Geographies geography={geoUrl}>
+                {
+                    ({ geographies }) => {
+                        // console.log(geographies);
 
-                                return (
-                                    <Geography
-                                        key={geo.rsmKey}
-                                        geography={geo}
-                                        // fill={getRedIntensityColor(index, minValue, maxValue)}
-                                        fill={getRedIntensityColor(index, 0, geographies.length)}
-                                    />
-                                );
+                        return geographies.map((geo, index) => {
+                            // const value = allData[index];
+                            const value = allData.find(item => {
+                                let location = item.capital;
+                                let country = location.split('-')[0].trim();
+                                return geo.properties.name === country;
+                            });
+                            let result;
+                            if (value?.average_temperature) {
+
+                                result = <Geography
+                                    key={geo.rsmKey}
+                                    geography={geo}
+                                    onClick={
+                                        () => {
+                                            toast(value.capital, {
+                                                style: {
+                                                    borderRadius: '10px',
+                                                    background: '#333',
+                                                    color: '#fbf',
+                                                    padding: '16px',
+                                                },
+                                            });
+                                            toast(value.average_temperature, {
+                                                style: {
+                                                    alignContent: 'center',
+                                                    borderRadius: '10px',
+                                                    background: '#333',
+                                                    color: '#fbf',
+                                                    padding: '16px',
+                                                }
+                                            });
+                                        }
+                                    }
+                                    // fill={getRedIntensityColor(index, minValue, maxValue)}
+                                    fill={getRedIntensityColor(value.average_temperature, minValue, maxValue)}
+                                />
+                            } else {
+                                result = <Geography
+                                    key={geo.rsmKey}
+                                    geography={geo}
+
+                                    fill={getRedIntensityColor(index, minValue, maxValue)}
+                                />
                             }
-
-                            )
+                            return result;
                         }
+
+                        )
                     }
-                </Geographies>
-            )}
-        </ComposableMap>
+                }
+            </Geographies>
+
+        </ComposableMap></div>
     );
 };
 
